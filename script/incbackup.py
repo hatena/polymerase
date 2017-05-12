8ac83f51f44e48f551d3a3d910d4322309365a9a
taku-k <taakuu19@gmail.com>
2017-05-12 17:50:29 +0900

    Add script/incbackup.py

--- script/incbackup.py
+++ script/incbackup.py
@@ -3,6 +3,16 @@
 import argparse
 import subprocess
 from string import Template
+import json
+
+try:
+    from urllib.parse import urlparse, urlencode
+    from urllib.request import urlopen, Request
+    from urllib.error import HTTPError
+except ImportError:
+    from urlparse import urlparse
+    from urllib import urlencode
+    from urllib2 import urlopen, Request, HTTPError
 
 cmd = Template("""
 xtrabackup \
@@ -12,12 +22,23 @@ xtrabackup \
   --password ${mysql_password} \
   --slave-info \
   --backup \
-  --stream=tar \
+  --incremental-lsn=${last_lsn} \
+  --stream=xbstream \
   | gzip -c \
   | curl -X POST \
     --data-binary \
-    @- 'http://${xtralab_host}:${xtralab_port}/api/${db}/full-backup'
+    @- 'http://${xtralab_host}:${xtralab_port}/api/${db}/inc-backup/${last_lsn}'
 """.strip())
+
+
+def get_last_lsn(args):
+    url = Template("http://${xtralab_host}:${xtralab_port}/api/${db}/last-lsn").substitute(vars(args))
+    try:
+        resp = urlopen(url)
+        return json.loads(resp.read().decode('utf8'))['last_lsn']
+    except HTTPError as e:
+        print(e)
+        return ""
 
 
 def main():
@@ -30,7 +51,13 @@ def main():
     parser.add_argument('--xtralab-port', dest='xtralab_port', type=int, default=80)
     parser.add_argument('--db', dest='db', type=str, required=True)
     args = parser.parse_args()
-    subprocess.call(cmd.substitute(vars(args)), shell=True)
+
+    last_lsn = get_last_lsn(args)
+    if last_lsn == "":
+        return
+    d = vars(args)
+    d['last_lsn'] = last_lsn
+    subprocess.call(cmd.substitute(d), shell=True)
 
 if __name__ == '__main__':
     main()

41121cdd95ffddb9b65e08096c393e5ed3df2575
Takuya Kuwahara <taakuu19@gmail.com>
2017-05-24 15:36:35 +0900

    Refactor pkg/cli/full_backup.go

--- pkg/cli/full_backup.go
+++ pkg/cli/full_backup.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"os"
 	"os/exec"
 
 	"github.com/codegangsta/cli"
@@ -18,38 +19,43 @@ var fullBackupFlag = cli.Command{
 	Action: runFullBackup,
 	Flags: []cli.Flag{
 		cli.StringFlag{Name: "mysql-host", Value: "localhost", Usage: "destination mysql host"},
-		cli.IntFlag{Name: "mysql-port", Value: 3306, Usage: "destination mysql port"},
+		cli.StringFlag{Name: "mysql-port", Value: "3306", Usage: "destination mysql port"},
 		cli.StringFlag{Name: "mysql-user", Usage: "destination mysql user"},
 		cli.StringFlag{Name: "mysql-password", Usage: "destination mysql password"},
 		cli.StringFlag{Name: "xtralab-host", Usage: "xtralab host"},
-		cli.IntFlag{Name: "xtralab-port", Value: 10110, Usage: "xtralab port"},
+		cli.StringFlag{Name: "xtralab-port", Value: "24925", Usage: "xtralab port"},
 		cli.StringFlag{Name: "db", Usage: "db name"},
 	},
 }
 
 func runFullBackup(c *cli.Context) {
-	//mysqlHost := c.String("mysql-host")
-	//mysqlPort := string(c.Int("mysql-port"))
-	//mysqlUser := c.String("mysql-user")
-	////mysqlPassword := c.String("mysql-password")
+	mysqlHost := c.String("mysql-host")
+	mysqlPort := c.String("mysql-port")
+	mysqlUser := c.String("mysql-user")
+	mysqlPassword := c.String("mysql-password")
 	xtralabHost := c.String("xtralab-host")
-	xtralabPort := string(c.String("xtralab-port"))
-	//db := c.String("db")
-	//
-	//c1 := exec.Command("xtrabackup", "--host", mysqlHost, "--port", mysqlPort, "--user", mysqlUser, "--slave-info", "--backup", "--stream=tar")
-	//c2 := exec.Command("gzip", "-c")
-	//
-	//r1, w1 := io.Pipe()
-	//c1.Stdout = w1
-	//c2.Stdin = r1
+	xtralabPort := c.String("xtralab-port")
+	db := c.String("db")
 
-	c2 := exec.Command("sh", "-c", "xtrabackup --host localhost --port 3306 --user root --slave-info --backup --stream=tar | gzip -c")
+	if db == "" {
+		fmt.Println("You should specify db")
+		os.Exit(1)
+	}
 
-	r2, w2 := io.Pipe()
+	var cmdSh string
+	if mysqlPassword != "" {
+		cmdSh = fmt.Sprintf("xtrabackup --host %s --port %s --user %s --password %s --slave-info --backup --stream=tar | gzip -c",
+			mysqlHost, mysqlPort, mysqlUser, mysqlPassword)
+	} else {
+		cmdSh = fmt.Sprintf("xtrabackup --host %s --port %s --user %s --slave-info --backup --stream=tar | gzip -c", mysqlHost, mysqlPort, mysqlUser)
+	}
+	cmd := exec.Command("sh", "-c", cmdSh)
 
-	c2.Stdout = w2
+	r, w := io.Pipe()
 
-	bufReader := bufio.NewReader(r2)
+	cmd.Stdout = w
+
+	buf := bufio.NewReader(r)
 
 	conn, err := grpc.Dial(fmt.Sprintf("%s:%s", xtralabHost, xtralabPort), grpc.WithInsecure())
 	if err != nil {
@@ -64,23 +70,19 @@ func runFullBackup(c *cli.Context) {
 	}
 
 	fmt.Println("Start xtrabackup")
-	//err = c1.Start()
-	//if err != nil {
-	//	panic(err)
-	//}
 	fmt.Println("Start gzip")
 	go func() {
-		err = c2.Start()
+		err = cmd.Start()
 		if err != nil {
 			panic(err)
 		}
-		c2.Wait()
-		w2.Close()
+		cmd.Wait()
+		w.Close()
 	}()
 
-	buf := make([]byte, 1024*1024)
+	chunk := make([]byte, 1024*1024)
 	for {
-		n, err := bufReader.Read(buf)
+		n, err := buf.Read(chunk)
 		if err == io.EOF {
 			reply, err := stream.CloseAndRecv()
 			if err != nil {
@@ -94,8 +96,8 @@ func runFullBackup(c *cli.Context) {
 		}
 		fmt.Printf("Read %d bytes\n", n)
 		stream.Send(&pb.FullBackupContentStream{
-			Content: buf[:n],
-			Db:      "test-db",
+			Content: chunk[:n],
+			Db:      db,
 		})
 	}
 }

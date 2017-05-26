a72d7ad4bf6d3d2d6bc0272041ce49a7656ddc17
taku-k <taakuu19@gmail.com>
2017-05-26 13:42:08 +0900

    Add pkg/utils/exec

---
+++ pkg/utils/exec/xtrabackup.go
@@ -0,0 +1,33 @@
+package exec
+
+import (
+	"bytes"
+	"os/exec"
+	"strings"
+	"text/template"
+)
+
+var fullBackupTmpl = strings.TrimSpace(`
+{{.BinPath}} \
+  --host {{.Host}} \
+  --port {{.Port}} \
+  --user {{.User}} \
+{{if .Password}}  --password {{.Password}} \{{end}}
+  --slave-info \
+  --backup \
+{{if .LsnTempDir}}  --extra-lsndir={{.LsnTempDir}} \{{end}}
+  --stream=tar
+`)
+
+func NewFullBackupCmd(cfg *XtrabackupConfig) (*exec.Cmd, error) {
+	err := cfg.InitDefaults()
+	if err != nil {
+		return nil, err
+	}
+	t := template.New("full_backup_cmd")
+	t, _ = t.Parse(fullBackupTmpl)
+	buf := new(bytes.Buffer)
+	t.Execute(buf, cfg)
+	cmd := exec.Command("sh", "-c", buf.String())
+	return cmd, nil
+}
---
+++ pkg/utils/exec/xtrabackup_test.go
@@ -0,0 +1,35 @@
+package exec
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestNewFullBackupCmd(t *testing.T) {
+	cfg := &XtrabackupConfig{
+		User:       "user",
+		Password:   "password",
+		LsnTempDir: "/tmp/test",
+	}
+	cfg.InitDefaults()
+	cmd, err := NewFullBackupCmd(cfg)
+
+	expected := []string{"sh", "-c", strings.TrimSpace(`
+xtrabackup \
+  --host 127.0.0.1 \
+  --port 3306 \
+  --user user \
+  --password password \
+  --slave-info \
+  --backup \
+  --extra-lsndir=/tmp/test \
+  --stream=tar`)}
+
+	if err != nil {
+		t.Errorf("Not failed: %v", err)
+	}
+	if !reflect.DeepEqual(cmd.Args, expected) {
+		t.Errorf("Command does not equal to expected command: actual=(%v) expected=(%v)", cmd.Args, expected)
+	}
+}

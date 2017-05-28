d1964595afb5ef7d18b92fa541fa99e39a286a99
Takuya Kuwahara <taakuu19@gmail.com>
2017-05-28 15:38:41 +0900

    Add test for pkg/util/envutil

--- pkg/utils/envutil/env.go
+++ pkg/utils/envutil/env.go
@@ -2,6 +2,8 @@ package envutil
 
 import "os"
 
+// EnvOrDefaultString returns the value set by the specified
+// environment variable, if any, otherwise the specified default value.
 func EnvOrDefaultString(name string, value string) string {
 	if v, found := os.LookupEnv(name); found {
 		return v

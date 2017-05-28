18f077d8d895fc4fc020780a8f530cb73083b205
Takuya Kuwahara <taakuu19@gmail.com>
2017-05-28 15:30:16 +0900

    Add tests for pkg/dirutil

--- pkg/utils/dirutil/dirutil.go
+++ pkg/utils/dirutil/dirutil.go
@@ -6,10 +6,12 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// MkdirAllWithLog creates path as a directory.
+// When creating the directory is failed, the path is logged.
 func MkdirAllWithLog(path string) error {
 	err := os.MkdirAll(path, 0755)
 	if err != nil {
-		log.WithField("path", path).Fatal("Cannot create directory")
+		log.WithField("path", path).Error("Cannot create directory")
 	}
 	return err
 }
---
+++ pkg/utils/dirutil/dirutil_test.go
@@ -0,0 +1,25 @@
+package dirutil
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestMkdirAllWithLogSuccess(t *testing.T) {
+	tempdir, _ := ioutil.TempDir("", "test")
+	defer os.RemoveAll(tempdir)
+
+	err := MkdirAllWithLog(filepath.Join(tempdir, "test"))
+	if err != nil {
+		t.Error("MkdirAllWithLog is failed")
+	}
+}
+
+func TestMkdirAllWithLogFailed(t *testing.T) {
+	err := MkdirAllWithLog("")
+	if err == nil {
+		t.Error("MkdirAllwithLog should be failed")
+	}
+}

458cbf6691a4bfdb910bfd2f786b6d1a9dcdf856
Takuya Kuwahara <taakuu19@gmail.com>
2017-05-29 00:10:17 +0900

    Add comments in pkg/server/config.go

--- pkg/server/config.go
+++ pkg/server/config.go
@@ -14,6 +14,7 @@ const (
 	defaultStoraPath = "polymerase-data"
 )
 
+// Config is a configuration for polymerase server.
 type Config struct {
 	*base.Config
 
@@ -27,6 +28,7 @@ type Config struct {
 	EtcdPeerPort string
 }
 
+// MakeConfig creates a initial Config.
 func MakeConfig() *Config {
 	cfg := &Config{
 		Config:        new(base.Config),
@@ -44,22 +46,28 @@ func MakeConfig() *Config {
 	return cfg
 }
 
+// TempDir returns a directory path for temporary.
 func (cfg *Config) TempDir() string {
 	return filepath.Join(cfg.StoreDir, "temp")
 }
 
+// LogsDir returns a directory path for log.
 func (cfg *Config) LogsDir() string {
 	return filepath.Join(cfg.StoreDir, "logs")
 }
 
+// BackupsDir returns a directory path for backup data.
 func (cfg *Config) BackupsDir() string {
 	return filepath.Join(cfg.StoreDir, "basckups")
 }
 
+// EtcdDataDir returns a directory path for etcd data dir.
 func (cfg *Config) EtcdDataDir() string {
 	return filepath.Join(cfg.StoreDir, "etcd")
 }
 
+// NewStoreDir returns an absolute path for value.
+// This does not accept '~'.
 func NewStoreDir(v string) (string, error) {
 	if len(v) == 0 {
 		return "", errors.New("no value specified")

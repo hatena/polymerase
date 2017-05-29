6077753f2c9ac04a3745dbb7481b48ef0a8b333f
Takuya Kuwahara <taakuu19@gmail.com>
2017-05-30 04:12:50 +0900

    Implement restore command

--- pkg/cli/restore.go
+++ pkg/cli/restore.go
@@ -1,14 +1,25 @@
 package cli
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
 
+	"github.com/cheggaaa/pb"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/taku-k/polymerase/pkg/base"
 	"github.com/taku-k/polymerase/pkg/storage/storagepb"
+	"github.com/taku-k/polymerase/pkg/utils/dirutil"
+	"github.com/taku-k/polymerase/pkg/utils/log"
+	"github.com/taku-k/polymerase/pkg/utils/exec"
+)
+
+const (
+	progressBarWidth = 80
 )
 
 type restoreContext struct {
@@ -46,10 +57,86 @@ func runRestore(cmd *cobra.Command, args []string) error {
 		Db:   db,
 		From: restoreCtx.from,
 	})
-	fmt.Fprintln(os.Stdout, res)
 
-	// TODO: implement the subsequent
-	fmt.Fprintln(os.Stdout, "Not implemented yet")
+	restoreDir, err := filepath.Abs("polymerase-restore")
+	if err != nil {
+		return err
+	}
+	if err := dirutil.MkdirAllWithLog(restoreDir); err != nil {
+		return err
+	}
+	log.Info("Restore data directory: ", restoreDir)
 
+	inc := len(res.Keys) - 1
+	idx := 0
+	for ; inc > 0; inc -= 1 {
+		if err := getIncBackup(scli, res.Keys[idx], restoreDir, inc); err != nil {
+			return err
+		}
+		idx += 1
+	}
+	if err := getFullBackup(scli, res.Keys[idx], restoreDir); err != nil {
+		return err
+	}
+	return nil
+}
+
+func getIncBackup(cli storagepb.StorageServiceClient, info *storagepb.BackupFileInfo, restoreDir string, inc int) error {
+	fn := filepath.Join(restoreDir, fmt.Sprintf("inc%d.xb.gz", inc))
+	if err := getBackup(cli, info, fn, fmt.Sprintf("inc%d | ", inc)); err != nil {
+		return err
+	}
+	cmd := exec.UnzipIncBackupCmd(context.TODO(), fn, restoreDir, inc)
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func getFullBackup(cli storagepb.StorageServiceClient, info *storagepb.BackupFileInfo, restoreDir string) error {
+	fn := filepath.Join(restoreDir, "base.tar.gz")
+	if err := getBackup(cli, info, fn, "base | "); err != nil {
+		return err
+	}
+	cmd := exec.UnzipFullBackupCmd(context.TODO(), fn, restoreDir)
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func getBackup(
+	cli storagepb.StorageServiceClient,
+	info *storagepb.BackupFileInfo,
+	fn string,
+	barPrefix string,
+) error {
+	stream, err := cli.GetFileByKey(context.Background(), &storagepb.GetFileByKeyRequest{
+		Key:         info.Key,
+		StorageType: info.StorageType,
+	})
+	if err != nil {
+		return err
+	}
+	f, err := os.Create(fn)
+	if err != nil {
+		return err
+	}
+	bufw := bufio.NewWriter(f)
+	bar := pb.New64(int64(info.Size)).SetUnits(pb.U_BYTES).Prefix(barPrefix)
+	bar.SetWidth(progressBarWidth)
+	bar.Start()
+	multiw := io.MultiWriter(bufw, bar)
+	for {
+		fs, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		multiw.Write(fs.Content)
+	}
+	bar.Finish()
 	return nil
 }

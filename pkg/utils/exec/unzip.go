f067ef305227e316dafc412e72a18b192311a2e0
Takuya Kuwahara <taakuu19@gmail.com>
2017-05-30 04:01:22 +0900

    Add unzip command utils

---
+++ pkg/utils/exec/unzip.go
@@ -0,0 +1,42 @@
+package exec
+
+import (
+	"context"
+	"os/exec"
+	"text/template"
+	"strings"
+	"bytes"
+	"strconv"
+)
+
+var unzipIncTmpl = strings.TrimSpace(`
+gunzip -c {{.name}} > {{.dir}}/inc{{.inc}}.xb && \
+  mkdir -p {{.dir}}/inc{{.inc}} && \
+  xbstream -x -C {{.dir}}/inc{{.inc}} < {{.dir}}/inc{{.inc}}.xb && \
+  rm -rf {{.dir}}/inc{{.inc}}.xb*
+`)
+
+var unzipFullTmpl = "mkdir {{.dir}}/base && tar xf {{.name}} -C {{.dir}}/base"
+
+func UnzipIncBackupCmd(ctx context.Context, name, dir string, inc int) *exec.Cmd {
+	t := template.New("unzip_inc_tmpl")
+	t, _ = t.Parse(unzipIncTmpl)
+	var buf bytes.Buffer
+	t.Execute(&buf, map[string]string{
+		"name": name,
+		"dir": dir,
+		"inc": strconv.Itoa(inc),
+	})
+	return exec.CommandContext(ctx, "sh", "-c", buf.String())
+}
+
+func UnzipFullBackupCmd(ctx context.Context, name, dir string) *exec.Cmd {
+	t := template.New("unzip_full_tmpl")
+	t, _ = t.Parse(unzipFullTmpl)
+	var buf bytes.Buffer
+	t.Execute(&buf, map[string]string{
+		"name": name,
+		"dir": dir,
+	})
+	return exec.CommandContext(ctx, "sh", "-c", buf.String())
+}
---
+++ pkg/utils/exec/unzip_test.go
@@ -0,0 +1,56 @@
+package exec
+
+import (
+	"testing"
+	"strings"
+	"context"
+	"reflect"
+)
+
+func TestUnzipIncBackupCmd(t *testing.T) {
+	var tests = []struct {
+		in []interface{}
+		expected []string
+	}{
+		{
+			in: []interface{}{"name", "dir", 1},
+			expected: []string{"sh", "-c", strings.TrimSpace(`
+gunzip -c name > dir/inc1.xb && \
+  mkdir -p dir/inc1 && \
+  xbstream -x -C dir/inc1 < dir/inc1.xb && \
+  rm -rf dir/inc1.xb*
+			`)},
+		},
+	}
+	for _, tt := range tests {
+		n := tt.in[0].(string)
+		d := tt.in[1].(string)
+		i := tt.in[2].(int)
+		cmd := UnzipIncBackupCmd(context.Background(), n, d, i)
+		if !reflect.DeepEqual(cmd.Args, tt.expected) {
+			t.Errorf("expected %v, got %v", tt.expected, cmd.Args)
+		}
+	}
+}
+
+func TestUnzipFullBackupCmd(t *testing.T) {
+	var tests = []struct {
+		in []string
+		expected []string
+	}{
+		{
+			in: []string{"name", "dir"},
+			expected: []string{"sh", "-c", strings.TrimSpace(`
+mkdir dir/base && tar xf name -C dir/base
+			`)},
+		},
+	}
+	for _, tt := range tests {
+		n := tt.in[0]
+		d := tt.in[1]
+		cmd := UnzipFullBackupCmd(context.Background(), n, d)
+		if !reflect.DeepEqual(cmd.Args, tt.expected) {
+			t.Errorf("expected %v, got %v", tt.expected, cmd.Args)
+		}
+	}
+}

5b281ecbb2a9ccf9725a64f31d7d9fbc6349ffb9
Takuya Kuwahara <taakuu19@gmail.com>
2017-05-29 16:07:25 +0900

    Join the existing cluster when join flag is on

--- pkg/etcd/etcd.go
+++ pkg/etcd/etcd.go
@@ -1,17 +1,24 @@
 package etcd
 
 import (
+	"bytes"
+	"context"
 	"fmt"
 	"net/url"
+	"time"
 
+	"github.com/coreos/etcd/clientv3"
 	"github.com/coreos/etcd/embed"
 	"github.com/pkg/errors"
+	"github.com/taku-k/polymerase/pkg/utils/log"
 )
 
 type EtcdContext struct {
 	ClientPort string
 	PeerPort   string
 	DataDir    string
+	JoinAddr   string
+	Name       string
 }
 
 func NewEtcdEmbedConfig(ctx *EtcdContext) (*embed.Config, error) {
@@ -42,9 +49,57 @@ func NewEtcdEmbedConfig(ctx *EtcdContext) (*embed.Config, error) {
 
 	etcdCfg.Dir = ctx.DataDir
 
-	etcdCfg.InitialCluster = etcdCfg.InitialClusterFromName("")
+	etcdCfg.Name = ctx.Name
+
+	if ctx.isInitialCluster() {
+		etcdCfg.InitialCluster = etcdCfg.InitialClusterFromName(ctx.Name)
+	} else {
+		cluster, err := ctx.AddMember(apurl.String())
+		if err != nil {
+			return nil, errors.Wrap(err, "AddMember API is failed")
+		}
+		log.Info(cluster)
+		etcdCfg.ClusterState = embed.ClusterStateFlagExisting
+		etcdCfg.InitialCluster = cluster
+	}
 
 	return etcdCfg, nil
+}
+
+func (c *EtcdContext) isInitialCluster() bool {
+	return c.JoinAddr == ""
+}
+
+func (c *EtcdContext) AddMember(peerUrl string) (string, error) {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{c.JoinAddr},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		return "", err
+	}
+	defer cli.Close()
+	res, err := cli.MemberAdd(context.Background(), []string{peerUrl})
+	if err != nil {
+		return "", err
+	}
+	log.Info(res.Members)
+	newID := res.Member.ID
+	var buf bytes.Buffer
+	//fmt.Fprintf(&buf, "%s=%s", res.Member.Name, peerUrl)
+	for _, m := range res.Members {
+		for _, u := range m.PeerURLs {
+			n := m.Name
+			if m.ID == newID {
+				n = c.Name
+			}
+			fmt.Fprintf(&buf, "%s=%s,", n, u)
+		}
+	}
+	if l := buf.Len(); l > 0 {
+		buf.Truncate(l - 1)
+	}
+	return buf.String(), nil
 }
 
 func NewEtcdServer(cfg *embed.Config) (*embed.Etcd, error) {

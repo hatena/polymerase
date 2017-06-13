e700736be2169d294322e233b3540943eaf48143
taku-k <taakuu19@gmail.com>
2017-06-13 14:36:45 +0900

    Add option desc

--- pkg/cli/flags.go
+++ pkg/cli/flags.go
@@ -103,8 +103,8 @@ func init() {
 		f.StringVar(&serverConnPort, "port", base.DefaultPort, "The port to bind to.")
 		f.StringVar(&serverCfg.StoreDir, "store-dir", serverCfg.StoreDir, "The dir path to store data files.")
 		f.StringVar(&serverCfg.JoinAddr, "join", "", "The address of node which acts as bootstrap when joining an existing cluster.")
-		f.StringVar(&serverCfg.EtcdPeerPort, "etcd-peer-port", "2380", "")
-		f.StringVar(&serverCfg.Name, "name", serverCfg.Name, "")
+		f.StringVar(&serverCfg.EtcdPeerPort, "etcd-peer-port", "2380", "The port to be used for etcd peer communication.")
+		f.StringVar(&serverCfg.Name, "name", serverCfg.Name, "The human-readable name.")
 	}
 
 	rootCmd.AddCommand(serverCmd, fullBackupCmd, incBackupCmd, restoreCmd)

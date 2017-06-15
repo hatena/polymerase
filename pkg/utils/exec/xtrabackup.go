46e9d11a396a5efcb7eec8d48cfdc197b1b4f28d
Takuya Kuwahara <taakuu19@gmail.com>
2017-06-15 11:27:30 +0900

    Add innobackupex template

--- pkg/utils/exec/xtrabackup.go
+++ pkg/utils/exec/xtrabackup.go
@@ -12,11 +12,17 @@ import (
 	"github.com/taku-k/polymerase/pkg/base"
 )
 
-var fullBackupTmpl = strings.TrimSpace(`
-{{.BinPath}} \
-  --host {{.Host}} \
-  --port {{.Port}} \
-  --user {{.User}} \{{ if .Password }}
+type backupCmd struct {
+	fullTmpl string
+	incTmpl  string
+}
+
+var xtrabackup = backupCmd{
+	fullTmpl: strings.TrimSpace(`
+{{ .BinPath }} \
+  --host {{ .Host }} \
+  --port {{ .Port }} \
+  --user {{ .User }} \{{ if .Password }}
   --password {{ .Password }} \
   {{- end }}
   --slave-info \
@@ -24,32 +30,70 @@ var fullBackupTmpl = strings.TrimSpace(`
   --extra-lsndir={{ .LsnTempDir }} \
   {{- end }}
   --stream=tar
-`)
-
-var incBackupTmpl = strings.TrimSpace(`
-{{.BinPath}} \
-  --host {{.Host}} \
-  --port {{.Port}} \
-  --user {{.User}} \{{ if .Password }}
-  --password {{.Password}} \
+`),
+	incTmpl: strings.TrimSpace(`
+{{ .BinPath }} \
+  --host {{ .Host }} \
+  --port {{ .Port }} \
+  --user {{ .User }} \{{ if .Password }}
+  --password {{ .Password }} \
   {{- end }}
   --slave-info \
   --backup \{{ if .LsnTempDir }}
   --extra-lsndir={{ .LsnTempDir }} \
   {{- end }}
   --stream=xbstream \
-  --incremental-lsn={{.ToLsn}}
-`)
+  --incremental-lsn={{ .ToLsn }}
+`),
+}
+
+var innobackupex = backupCmd{
+	fullTmpl: strings.TrimSpace(`
+{{ .BinPath }} \
+  --host {{ .Host }} \
+  --port {{ .Port }} \
+  --user {{ .User }} \{{ if .Password }}
+  --password {{ .Password }} \
+  {{- end }}
+  --slave-info \
+  --extra-lsndir={{ .LsnTempDir }} \
+  --stream=tar
+`),
+	incTmpl: strings.TrimSpace(`
+{{ .BinPath }} \
+  --host {{ .Host }} \
+  --port {{ .Port }} \
+  --user {{ .User }} \{{ if .Password }}
+  --password {{ .Password }} \
+  {{- end }}
+  --slave-info \
+  --extra-lsndir={{ .LsnTempDir }} \
+  --stream=xbstream \
+  --incremental-lsn={{ .ToLsn }}
+`),
+}
 
 func BuildFullBackupCmd(ctx context.Context, cfg *base.XtrabackupConfig) (*exec.Cmd, error) {
-	return _buildBackupCmd(ctx, cfg, fullBackupTmpl)
+	var tmpl string
+	if cfg.UseInnobackupex {
+		tmpl = innobackupex.fullTmpl
+	} else {
+		tmpl = xtrabackup.fullTmpl
+	}
+	return _buildBackupCmd(ctx, cfg, tmpl)
 }
 
 func BuildIncBackupCmd(ctx context.Context, cfg *base.XtrabackupConfig) (*exec.Cmd, error) {
 	if cfg.ToLsn == "" {
 		return nil, errors.New("ToLSN cannot be empty")
 	}
-	return _buildBackupCmd(ctx, cfg, incBackupTmpl)
+	var tmpl string
+	if cfg.UseInnobackupex {
+		tmpl = innobackupex.incTmpl
+	} else {
+		tmpl = xtrabackup.incTmpl
+	}
+	return _buildBackupCmd(ctx, cfg, tmpl)
 }
 
 func PrepareBaseBackup(ctx context.Context, cfg *base.XtrabackupConfig) *exec.Cmd {

aeb037106da0ce11dd2e84c5e7192f8d77294cd4
Takuya Kuwahara <taakuu19@gmail.com>
2017-06-20 23:50:39 +0900

    Add pkg/allocator/search.go

---
+++ pkg/allocator/search.go
@@ -0,0 +1,24 @@
+package allocator
+
+import (
+	"github.com/coreos/etcd/clientv3"
+	"github.com/golang/protobuf/proto"
+	"github.com/pkg/errors"
+	"github.com/taku-k/polymerase/pkg/base"
+	"github.com/taku-k/polymerase/pkg/storage/storagepb"
+)
+
+func SearchStoredAddr(cli *clientv3.Client, db string) (string, error) {
+	res, err := cli.KV.Get(cli.Ctx(), base.BackupDBKey(db))
+	if err != nil {
+		return "", err
+	}
+	if len(res.Kvs) == 0 {
+		return "", errors.New("DB not found.")
+	}
+	info := &storagepb.BackupInfo{}
+	if err := proto.Unmarshal(res.Kvs[0].Value, info); err != nil {
+		return "", err
+	}
+	return info.StoredHost, nil
+}

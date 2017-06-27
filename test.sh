993c8f58000322c03de1a869354f9cbe2870cc49
taku-k <taakuu19@gmail.com>
2017-06-27 15:25:13 +0900

    Use MySQL Sandbox instead of mysql-server

--- test.sh
+++ test.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
 # Launch MySQL server
-service mysql start
+/root/sandboxes/start_all
 
 # Start benchmark
 
 # Do full-backup
-polymerase full-backup --mysql-user root --mysql-password root --db test-db --host server
+polymerase \
+  full-backup \
+  --mysql-port 23701 \
+  --mysql-user msandbox \
+  --mysql-password msandbox \
+  --db test-db \
+  --host server
 
 # Do inc-backup
+polymerase \
+  inc-backup \
+  --mysql-port 23701 \
+  --mysql-user msandbox \
+  --mysql-password msandbox \
+  --db test-db \
+  --host server
 
 # Do restore

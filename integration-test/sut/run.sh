8314f10d50f874b27cdf62cd081510c365d28251
taku-k <taakuu19@gmail.com>
2017-06-27 18:49:27 +0900

    Setup ycsb in Dockerfile.test

--- integration-test/sut/run.sh
+++ integration-test/sut/run.sh
@@ -4,6 +4,7 @@
 /root/sandboxes/start_all
 
 # Start benchmark
+# bin/ycsb run jdbc -P workloads/workloada -p db.driver=com.mysql.jdbc.Driver -p db.url=jdbc:mysql://127.0.0.1:23700/testdb -p db.user=msandbox -p db.passwd=msandbox -target 10 &
 
 # Do full-backup
 polymerase \

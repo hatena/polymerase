9d29fbcaa40a53c8287940eb759cb82e0c6dbc97
taku-k <taakuu19@gmail.com>
2017-06-30 21:34:53 +0900

    Output nodes info as json

--- pkg/cli/info.go
+++ pkg/cli/info.go
@@ -8,7 +8,7 @@ import (
 	"time"
 
 	"github.com/coreos/etcd/clientv3"
-	"github.com/dustin/go-humanize"
+	"github.com/gogo/protobuf/jsonpb"
 	"github.com/spf13/cobra"
 	"github.com/taku-k/polymerase/pkg/status"
 	"github.com/taku-k/polymerase/pkg/status/statuspb"
@@ -30,28 +30,16 @@ func runNodesInfo(cmd *cobra.Command, args []string) error {
 	}
 	defer cli.Close()
 
-	kv := status.GetNodesInfo(cli)
-
-	for n, info := range kv {
-		outputNodeInfo(n, info)
-		fmt.Fprintln(os.Stdout, "")
+	nodes := status.GetNodesInfo(cli)
+	marshaler := jsonpb.Marshaler{
+		Indent: "  ",
 	}
-
+	json, err := marshaler.MarshalToString(nodes)
+	if err != nil {
+		return err
+	}
+	fmt.Fprint(os.Stdout, json)
 	return nil
-}
-
-func outputNodeInfo(node string, info *statuspb.NodeInfo) {
-	var buf bytes.Buffer
-	tw := tabwriter.NewWriter(&buf, 2, 1, 2, ' ', 0)
-	fmt.Fprintf(tw, "Node:\t%s\n", node)
-	fmt.Fprintln(tw, "===============")
-	fmt.Fprintf(tw, "Total:\t%s\n", humanize.Bytes(info.DiskInfo.Total))
-	fmt.Fprintf(tw, "Avail:\t%s\n", humanize.Bytes(info.DiskInfo.Avail))
-	if err := tw.Flush(); err != nil {
-		return
-	}
-	msg := buf.String()
-	fmt.Fprint(os.Stdout, msg)
 }
 
 var backupsInfoCmd = &cobra.Command{
@@ -85,7 +73,10 @@ func outputBackupInfo(db string, info *statuspb.BackupInfo) {
 	tw := tabwriter.NewWriter(&buf, 2, 1, 2, ' ', 0)
 	fmt.Fprintf(tw, "DB:\t%s\n", db)
 	fmt.Fprintln(tw, "===============")
-	fmt.Fprintf(tw, "FullBackup:\n\tNode:%s\n\tHost:%s\n", info.FullBackup.NodeName, info.FullBackup.Host)
+	fmt.Fprintf(tw, "FullBackup:\nNode:\t%s\nHost:\t%s\nStored Time:\t%v\n",
+		info.FullBackup.NodeName,
+		info.FullBackup.Host,
+		time.Unix(info.FullBackup.StoredTime, 0).Format(baseCfg.TimeFormat))
 	if len(info.IncBackups) != 0 {
 		fmt.Fprintln(tw, "\tIncBackup:")
 		for _, i := range info.IncBackups {

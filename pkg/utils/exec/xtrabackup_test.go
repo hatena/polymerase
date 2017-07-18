39057758aa7cf2fd85313b301070df5e67ca3b40
taku-k <taakuu19@gmail.com>
2017-07-18 16:28:10 +0900

    Pass xtrabackup command test

--- pkg/utils/exec/xtrabackup_test.go
+++ pkg/utils/exec/xtrabackup_test.go
@@ -22,6 +22,7 @@ func TestBuildFullBackupCmd(t *testing.T) {
 				Password:     "password",
 				LsnTempDir:   "/tmp/test",
 				InsecureAuth: true,
+				Parallel:     1,
 			},
 			[]string{"sh", "-c", strings.TrimSpace(`
 xtrabackup \
@@ -31,16 +32,18 @@ xtrabackup \
   --password password \
   --slave-info \
   --backup \
-  --extra-lsndir=/tmp/test \
+  --extra-lsndir /tmp/test \
   --skip-secure-auth \
   --safe-slave-backup \
-  --stream=tar
+  --stream tar \
+  --parallel 1
   			`)},
 		}, {
 			&base.XtrabackupConfig{
 				BinPath:    "/usr/bin/xtrabackup",
 				User:       "user",
 				LsnTempDir: "/tmp/test",
+				Parallel:   1,
 			},
 			[]string{"sh", "-c", strings.TrimSpace(`
 /usr/bin/xtrabackup \
@@ -49,9 +52,10 @@ xtrabackup \
   --user user \
   --slave-info \
   --backup \
-  --extra-lsndir=/tmp/test \
+  --extra-lsndir /tmp/test \
   --safe-slave-backup \
-  --stream=tar
+  --stream tar \
+  --parallel 1
 			`)},
 		},
 	}
@@ -78,6 +82,7 @@ func TestBuildIncBackupCmd(t *testing.T) {
 		Password:   "password",
 		LsnTempDir: "/tmp/test",
 		ToLsn:      "100",
+		Parallel:   1,
 	}
 	cfg.InitDefaults()
 	defer os.RemoveAll(cfg.LsnTempDir)
@@ -92,10 +97,11 @@ xtrabackup \
   --password password \
   --slave-info \
   --backup \
-  --extra-lsndir=/tmp/test \
-  --stream=xbstream \
+  --extra-lsndir /tmp/test \
+  --stream xbstream \
   --safe-slave-backup \
-  --incremental-lsn=100`)}
+  --incremental-lsn 100 \
+  --parallel 1`)}
 
 	if err != nil {
 		t.Errorf("Not failed: %v", err)
